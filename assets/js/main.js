952c7edf7c660154a8c1ce2fea7624d455c85c44
John D. Storey <johndangerstorey@gmail.com>
2016-05-15 23:55:19 -0600

    updates xobelize to tinybelize

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,7 @@
 
 			// Menu settings
 			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
+				$('.menu').toggleClass('menu-open');
 			});
 
 			// Smooth Scrolling w/o any other plugins
